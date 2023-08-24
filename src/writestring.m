d77d8eb44225caf734e7502f217a42e094396053
P. Talley <otaithleigh@users.noreply.github.com>
2023-08-24 12:09:59 -0400

    Add encoding option to writestring

--- src/writestring.m
+++ src/writestring.m
@@ -9,16 +9,23 @@ function writestring(s, filename, options)
 %       file FILENAME, joining elements of S with DELIMITER before writing.
 %       (default: "\n")
 %
+%   WRITESTRING(..., 'Encoding', ENCODING) writes the string S to the file
+%       FILENAME using the character encoding ENCODING. See the FOPEN docs
+%       for available encodings. (default: "UTF-8")
+%
 %   WRITESTRING(..., 'ForceFinalNewline', FORCE) controls whether the joined
 %       string is forced to have a final "\n" character or not. (default: true)
 %
 %   WRITESTRING(..., 'Mode', MODE) selects between append ('a') and overwrite
 %       ('w') mode for writing to the file. (default: 'w')
 %
+%   See also: fopen
+%
 arguments
     s (:,1) string
     filename (1,1) string
     options.Delimiter string = "\n"
+    options.Encoding (1,1) string = "UTF-8"
     options.ForceFinalNewline (1,1) logical = true
     options.Mode (1,1) string {mustBeMember(options.Mode, ["a", "w"])} = "w"
 end
@@ -28,7 +35,7 @@ if options.ForceFinalNewline && ~endsWith(stringToWrite, newline)
     stringToWrite = append(stringToWrite, newline);
 end
 
-fid = fopen(filename, options.Mode);
+fid = fopen(filename, options.Mode, 'native', options.Encoding);
 cleanup = onCleanup(@()fclose(fid));
 if fid == -1
     error('Could not open %s for writing', filename)
